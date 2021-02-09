411fe828e55c786d0f2aef0fef291ce9e793c6d9
Roopesh Saravanan <helloiamroopesh@gmail.com>
2021-02-09 08:53:42 +0530

    files commited
    project files updated and commited

--- script.js
+++ script.js
@@ -1,36 +1,112 @@
 const MAPBOX_ACCESS_TOKEN =
   'pk.eyJ1IjoidGhlcm9vcGVzaCIsImEiOiJja2t1eWJ2bjIwejA3MzFsbWo0Mmt5b3hxIn0.CVI29oMR3N-po-cNuBal-w'
 
+// var map = new mapboxgl.Map({
+//   accessToken: MAPBOX_ACCESS_TOKEN,
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/light-v10',
+//   center: [78.9629, 20.5937],
+//   zoom: 3,
+// })
+
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
   enableHighAccuracy: true,
 })
+
+// Location that is captured by location sensor
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+// Error Location
+function errorLocation() {
+  errorMap()
+}
 
 function setupMap(centerPosition) {
   const map = new mapboxgl.Map({
     accessToken: MAPBOX_ACCESS_TOKEN,
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: centerPosition,
     zoom: 14,
   })
 
-  const navigationControls = new mapboxgl.NavigationControl()
-  map.addControl(navigationControls)
+  var geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: centerPosition,
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Your Location',
+        },
+      },
+    ],
+  }
 
-  const directionControls = new MapboxDirections({
-    accessToken: MAPBOX_ACCESS_TOKEN,
+  geojson.features.forEach(function (marker) {
+    // create a HTML element for each feature
+    var el = document.createElement('div')
+    el.className = 'marker'
+
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(
+            '<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'
+          )
+      )
+      .addTo(map)
   })
-  map.addControl(directionControls, 'top-left')
 }
 
-//Location that is captured by location sensor
-function successLocation(position) {
-  setupMap([position.coords.longitude, position.coords.latitude])
-  console.log(position.coords.longitude)
-  console.log(position.coords.latitude)
-}
+function errorMap() {
+  const map = new mapboxgl.Map({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [78.9629, 20.5937],
+    zoom: 4,
+  })
 
-// Error Location
-function errorLocation() {
-  setupMap([80.2707, 13.0827])
+  // var geojson = {
+  //   type: 'FeatureCollection',
+  //   features: [
+  //     {
+  //       type: 'Feature',
+  //       geometry: {
+  //         type: 'Point',
+  //         coordinates: [78.9629, 20.5937],
+  //       },
+  //       properties: {
+  //         title: 'Mapbox',
+  //         description: 'Location cannot be mapped',
+  //       },
+  //     },
+  //   ],
+  // }
+
+  // geojson.features.forEach(function (marker) {
+  //   // create a HTML element for each feature
+  //   var el = document.createElement('div')
+  //   el.className = 'marker'
+
+  //   // make a marker for each feature and add to the map
+  //   new mapboxgl.Marker(el)
+  //     .setLngLat(marker.geometry.coordinates)
+  //     .setPopup(
+  //       new mapboxgl.Popup({ offset: 25 }) // add popups
+  //         .setHTML(
+  //           '<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'
+  //         )
+  //     )
+  //     .addTo(map)
+  // })
 }
